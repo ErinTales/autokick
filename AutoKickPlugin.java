204ae795c06f6919099b6b2b6fbf14abdf66c222
ErinTales <45501985+ErinTales@users.noreply.github.com>
2020-12-17 21:21:12 -0600

    Update AutoKickPlugin.java
    Removed idiot code, added ::kick command.

--- AutoKickPlugin.java
+++ AutoKickPlugin.java
@@ -4,8 +4,10 @@ import com.google.inject.Provides;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.ScriptID;
+import net.runelite.api.events.CommandExecuted;
 import net.runelite.api.events.FriendsChatMemberJoined;
 import net.runelite.api.events.ScriptCallbackEvent;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
@@ -28,13 +30,12 @@ public class AutoKickPlugin extends Plugin {
     private Client client;
 
     @Inject
+    private ClientThread clientThread;
+
+    @Inject
     private AutoKickConfig config;
 
     private List<String> blacklistedNames;
-
-    private boolean suppressKickMessage = false;
-    private boolean suppressingNextKick = false;
-
 
     @Provides //I always forget this...
     AutoKickConfig provideConfig(ConfigManager configManager)
@@ -46,15 +47,13 @@ public class AutoKickPlugin extends Plugin {
     protected void startUp()
     {
         blacklistedNames = Text.fromCSV(config.blacklist().toLowerCase());
-        suppressKickMessage = config.suppressBlacklistKick();
     }
 
     @Subscribe
     public void onConfigChanged(ConfigChanged configChanged)
     {
-        //Both of these may be redundant.
+        //This may be redundant.
         blacklistedNames = Text.fromCSV(config.blacklist().toLowerCase());
-        suppressKickMessage = config.suppressBlacklistKick();
     }
 
     @Subscribe
@@ -67,10 +66,6 @@ public class AutoKickPlugin extends Plugin {
             client.addChatMessage(ChatMessageType.ENGINE, "",
                     getColorTag(config.messageColor()) + "Attempting to kick " + nameBlacklist + " from friends chat...", "");
 
-            if(suppressKickMessage)
-            {
-                suppressingNextKick = true;
-            }
             client.runScript(ScriptID.FRIENDS_CHAT_SEND_KICK, nameBlacklist);
         }
     }
@@ -84,20 +79,40 @@ public class AutoKickPlugin extends Plugin {
     @Subscribe
     public void onScriptCallbackEvent(ScriptCallbackEvent scriptCallbackEvent)
     {
-        if(scriptCallbackEvent.getEventName().equals("sendKickName"))
+        if(scriptCallbackEvent.getEventName().equals("sendKickName") && config.suppressKick())
         {
-
+            //This *WILL NOT WORK* without modification to the file FriendsChatSendKick.rs2asm
             final String[] stringStack = client.getStringStack();
             final int stringSize = client.getStringStackSize();
 
-            //This *WILL NOT WORK* without modification to the file FriendsChatSendKick.rs2asm
-            if(suppressingNextKick)
-            {
-                //Deletes the message entirely so it won't be displayed.
-                stringStack[stringSize - 2] = "";
-            }
-
-            suppressingNextKick = false;
+            //Deletes the message entirely so it won't be displayed.
+            stringStack[stringSize - 2] = "";
         }
+    }
+
+
+    @Subscribe
+    public void onCommandExecuted(CommandExecuted commandExecuted)
+    {
+        if (!commandExecuted.getCommand().equalsIgnoreCase("kick") || !config.kickCommand())
+        {
+            return;
+        }
+
+        String[] args = commandExecuted.getArguments();
+        StringBuilder name = new StringBuilder();
+
+        for(String s : args) //names with spaces will be read as having multiple args, so we need to iterate through them.
+        {
+            name.append(s);
+            name.append(" ");
+        }
+        name = new StringBuilder(name.toString().trim());
+
+        client.addChatMessage(ChatMessageType.ENGINE, "",
+                getColorTag(config.messageColor()) + "Attempting to kick " + name + " from friends chat...", "");
+
+        StringBuilder finalName = name;
+        clientThread.invokeLater(() -> client.runScript(ScriptID.FRIENDS_CHAT_SEND_KICK, finalName.toString()));
     }
 }
