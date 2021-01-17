3914f1e39f98b68c12c03bcdf52f8b5981e52963
ErinTales <45501985+ErinTales@users.noreply.github.com>
2021-01-16 19:03:29 -0600

    Update AutoKickPlugin.java

--- AutoKickPlugin.java
+++ AutoKickPlugin.java
@@ -4,9 +4,7 @@ import com.google.inject.Provides;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.ScriptID;
-import net.runelite.api.events.CommandExecuted;
-import net.runelite.api.events.FriendsChatMemberJoined;
-import net.runelite.api.events.ScriptCallbackEvent;
+import net.runelite.api.events.*;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -73,12 +71,6 @@ public class AutoKickPlugin extends Plugin {
         }
     }
 
-    private String getColorTag(Color color) //This is probably idiot code. Not sure if it's me or Jagex that's the idiot though.
-    {
-        String hex = String.format("%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
-        return "<col=" + hex + ">";
-    }
-
     @Subscribe
     public void onScriptCallbackEvent(ScriptCallbackEvent scriptCallbackEvent)
     {
@@ -95,31 +87,50 @@ public class AutoKickPlugin extends Plugin {
         }
     }
 
-
     @Subscribe
     public void onCommandExecuted(CommandExecuted commandExecuted)
     {
-        if (!commandExecuted.getCommand().equalsIgnoreCase("kick") || !config.kickCommand())
-        {
-            return;
+        if(commandExecuted.getCommand().equalsIgnoreCase("kickall") && config.kickAllCommand()) {
+            for (String s : blacklistedNames) {
+
+                clientThread.invokeLater(() ->
+                {
+                    suppressKick = true;
+                    client.runScript(ScriptID.FRIENDS_CHAT_SEND_KICK, s);
+                });
+            }
+
+            client.addChatMessage(ChatMessageType.ENGINE, "",
+                    getColorTag(config.messageColor()) + "Attempting to kick everyone on your blacklist...", "");
         }
-
-        String[] args = commandExecuted.getArguments();
-        StringBuilder name = new StringBuilder();
-
-        for(String s : args) //names with spaces will be read as having multiple args, so we need to iterate through them.
+        else if(commandExecuted.getCommand().equalsIgnoreCase("kick") && config.kickCommand())
         {
-            name.append(s);
-            name.append(" ");
+            String[] args = commandExecuted.getArguments();
+            StringBuilder name = new StringBuilder();
+
+            for(String s : args) //names with spaces will be read as having multiple args, so we need to iterate through them.
+            {
+                name.append(s);
+                name.append(" ");
+            }
+            name = new StringBuilder(name.toString().trim());
+
+            client.addChatMessage(ChatMessageType.ENGINE, "",
+                    getColorTag(config.messageColor()) + "Attempting to kick " + name + " from friends chat...", "");
+
+            StringBuilder finalName = name;
+
+            if(config.suppressKick()) {
+                suppressKick = true;
+            }
+
+            clientThread.invokeLater(() -> client.runScript(ScriptID.FRIENDS_CHAT_SEND_KICK, finalName.toString()));
         }
-        name = new StringBuilder(name.toString().trim());
+    }
 
-        client.addChatMessage(ChatMessageType.ENGINE, "",
-                getColorTag(config.messageColor()) + "Attempting to kick " + name + " from friends chat...", "");
-
-        StringBuilder finalName = name;
-        suppressKick = true;
-
-        clientThread.invokeLater(() -> client.runScript(ScriptID.FRIENDS_CHAT_SEND_KICK, finalName.toString()));
+    private String getColorTag(Color color) //This is probably idiot code. Not sure if it's me or Jagex that's the idiot though.
+    {
+        String hex = String.format("%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
+        return "<col=" + hex + ">";
     }
 }
