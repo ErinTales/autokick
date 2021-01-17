cd11aeec2e10af551bc16305b1b7f50026c8b334
ErinTales <45501985+ErinTales@users.noreply.github.com>
2021-01-16 19:04:58 -0600

    Update AutoKickConfig.java

--- AutoKickConfig.java
+++ AutoKickConfig.java
@@ -12,12 +12,12 @@ public interface AutoKickConfig extends Config
     @ConfigItem(
             keyName = "blacklist",
             name = "Blacklisted Names",
-            description = "Blacklisted Names. Format: '[name], [name], ...'",
+            description = "Blacklisted Names. Format: '[name], [name], ...' Replace -s and _s with spaces.",
             position = 1
     )
     default String blacklist() { return ""; }
 
-    @ConfigItem( //Currently reenabled, but requires modification of FriendsChatSendKick.rs2asm
+    @ConfigItem( //Requires modification of FriendsChatSendKick.rs2asm
             keyName = "suppressKick",
             name = "Suppress Kick Messages",
             description = "Suppress the kick messages for users who you kicked via this plugin.",
@@ -28,16 +28,28 @@ public interface AutoKickConfig extends Config
     @ConfigItem(
             keyName = "kickCommand",
             name = "Enable kick command",
-            description = "Enables the ::kick [username] command",
+            description = "Enables the ::kick [username] command.",
             position = 3
     )
     default boolean kickCommand() { return false; }
 
     @ConfigItem(
+            keyName = "kickAllCommand",
+            name = "Enable kick all command",
+            description = "Enables the ::kickall command, to kick everyone on the blacklist. " +
+                    "Please be warned that this command will generate a lot of kick messages, " +
+                    "so it is recommended that Suppress Kick Messages is enabled when using this " +
+                    "command. In addition, it will still generate a 'Your request to kick/ban user...' " +
+                    "message for each person kicked.",
+            position = 4
+    )
+    default boolean kickAllCommand() { return false; }
+
+    @ConfigItem(
             keyName = "messageColor",
             name = "Warning Message Color",
             description = "Color for the message that displays who you kicked.",
-            position = 4
+            position = 5
     )
     default Color messageColor() { return Color.MAGENTA; }
 
